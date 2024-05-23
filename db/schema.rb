c1fe87f3c3a7f8f5081fdae0b3de900dadcec2d4
2024-TAKUMI <ec2-user@ip-172-31-44-129.ap-northeast-1.compute.internal>
2024-05-23 06:50:58 +0000

    [Update]Bookers2

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_05_22_134805) do
+ActiveRecord::Schema.define(version: 2024_05_23_062406) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -41,13 +41,11 @@ ActiveRecord::Schema.define(version: 2024_05_22_134805) do
   end
 
   create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.text "caption"
+    t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "user_id"
-    t.string "title"
-    t.text "body"
-    t.string "other_column"
-    t.index ["user_id"], name: "index_books_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
